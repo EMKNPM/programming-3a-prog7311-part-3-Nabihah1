eedc0219ccf2e2236565917eebae74215ff581d3
Nabihah Sema <ST10440590@myemeris.edu.za>
2026-04-21 20:59:17 +0200

    Added DataContext,  Scaffolded & Updated Home Page Layout

--- PROG7311TechMoveLogistics/Program.cs
+++ PROG7311TechMoveLogistics/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using PROG7311TechMoveLogistics.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+//add this to connect to the database 
+builder.Services.AddDbContext<DataContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+
 
 var app = builder.Build();
 
